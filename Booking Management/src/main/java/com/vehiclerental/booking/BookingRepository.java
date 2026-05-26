506fc493e28b8fe0668489bf71617c361b2781d8
IT25100918 <it25100918@my.sliit.lk>
2026-05-26 13:14:39 +0530

    BookingRepository.java

--- Booking Management/src/main/java/com/vehiclerental/booking/BookingRepository.java
+++ Booking Management/src/main/java/com/vehiclerental/booking/BookingRepository.java
@@ -4,10 +4,11 @@ import com.vehiclerental.shared.FileRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
+    //inheritance
 public class BookingRepository extends FileRepository<Booking> {
 
     private static final String FILE_PATH = "data/bookings.txt";
-
+//polymorphysm
     @Override protected String getFilePath() { return FILE_PATH; }
     @Override protected Booking fromLine(String line) { return Booking.fromFileString(line); }
     @Override protected String toLine(Booking b) { return b.toFileString(); }
